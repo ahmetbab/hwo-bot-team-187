36ab786230309d1fe173b079e80181f45ab8db7d
Mika Urtela <mika.urtela@soulaimstudios.com>
2012-09-10 20:22:39 +0300

    And added the other files as well.

--- redlynx/pong/PongGameBot.java
+++ redlynx/pong/PongGameBot.java
@@ -8,18 +8,24 @@ public abstract class PongGameBot {
     private final PongGameCommunicator communicator;
 
     protected abstract String getName();
+
+    // TODO: Create base implementation. Abstraction should happen on a more detailed level.
     protected abstract void onReceivedJSONString(String serverMessage);
+
+    // TODO: Create basic upkeep of game state. Add an abstract tick call, in case bot wants
+    // TODO: to add some actions to the basic upkeep phase.
     protected abstract void onTick(long time);
 
     public PongGameBot(PongGameCommunicator communicator, ArrayBlockingQueue<String> serverMessageQueue) {
         this.serverMessageQueue = serverMessageQueue;
         this.communicator = communicator;
-
-        communicator.sendJoin(getName());
     }
 
 
     public void start() {
+
+        this.communicator.sendJoin(getName());
+
         while(true) {
             String serverMessage = serverMessageQueue.poll();
             if(serverMessage != null) {
@@ -29,7 +35,7 @@ public abstract class PongGameBot {
             onTick(System.currentTimeMillis());
 
             try {
-                Thread.sleep(10);
+                Thread.sleep(5);
             } catch (InterruptedException e) {
                 // if interrupted, exit program.
                 break;
--- redlynx/pong/PongGameCommunicator.java
+++ redlynx/pong/PongGameCommunicator.java
@@ -1,4 +1,22 @@
 package redlynx.pong;
 
+import java.io.PrintStream;
+
 public class PongGameCommunicator {
+
+    private final PrintStream out;
+
+    public PongGameCommunicator(PrintStream out) {
+        this.out = out;
+    }
+
+    public void sendJoin(String data) {
+        out.print("{\"msgType\":\"join\",\"data\":\"" + data + "\"}");
+        out.flush();
+    }
+
+    public void sendUpdate(float data) {
+        out.print("{\"msgType\":\"changeDir\",\"data\":\"" + data + "\"}");
+        out.flush();
+    }
 }
