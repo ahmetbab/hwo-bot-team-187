53ffe05742b5697e39cd6f70fe1b52ff3a8a0334
Mika Urtela <mika.urtela@soulaimstudios.com>
2012-09-20 13:40:42 +0300

    Added build run stop scripts.

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Building.."
+ant -q
+echo "Done."
---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function run
+{
+	local REQUIRED_ARG_COUNT=3
+
+	if [ $# -lt $REQUIRED_ARG_COUNT ]
+	then
+		echo "this script requires three parameters. not enough parameters provided"
+		echo "[name] [server] [port]"
+	else
+		echo "Starting bot with name $1"
+		echo "Connecting to $2:$3"
+		java out/production/pongbot/redlynx/bots/magmus/Magmus $1 $2 $3 &
+		echo $! > .pids
+	fi
+}
+
+run "$@"
